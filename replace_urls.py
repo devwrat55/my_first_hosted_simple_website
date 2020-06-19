9bfb81bcfc18ff8e4b900189ebf05dc4971ab4f0
Devwrat Nevewani <44137372+devwrat55@users.noreply.github.com>
2020-06-19 17:56:38 +0530

    page2_fixed

--- replace_urls.py
+++ replace_urls.py
@@ -13,8 +13,22 @@ def replacingtexts(old1,new1):
 		os.rename('/var/www/html/index_copy.html', '/var/www/html/index.html')	
 	except:
 		pass
+	
+	try:
+		f1 = open('/var/www/html/script.js', 'r')
+		f2 = open('/var/www/html/script_copy.js', 'w')
+		for line in f1:
+			f2.write(line.replace(old1, new1))
+		f1.close()
+		f2.close()
+
+		import os
+		os.remove("/var/www/html/script.js")
+		os.rename('/var/www/html/script_copy.js', '/var/www/html/script.js')	
+	except:
+		pass
 
 if __name__ == "__main__":
-    	new = str(sys.argv[1])
-    	old = "images"
-    	replacingtexts(old, new)
+    		new = str(sys.argv[1])
+    		old = "images"
+    		replacingtexts(old, new)
