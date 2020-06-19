b1236f081fa5f8f03383411e349df6bd376f461c
Devwrat Nevewani <44137372+devwrat55@users.noreply.github.com>
2020-06-19 12:57:17 +0530

    python program to replace url in html file

---
+++ replace_urls.py
@@ -0,0 +1,18 @@
+import sys
+
+def relacingtexts(old,new):
+    with open('/var/www/html/index.html', 'r') as file :
+  		filedata = file.read()
+	
+	# Replace the target string
+	filedata = filedata.replace(old, new)
+	
+	# Write the file out again
+	with open('/var/www/html/index.html', 'w') as file:
+	  file.write(filedata)
+
+
+if __name__ == "__main__":
+    new = str(sys.argv[1])
+    old = "images"
+    relacingtexts(old, new)
