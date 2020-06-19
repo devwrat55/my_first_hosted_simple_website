6a375df8b7c9a1b6f824f4067dc1a6145fb97279
Devwrat Nevewani <44137372+devwrat55@users.noreply.github.com>
2020-06-19 14:25:28 +0530

    updated_complete_file

--- replace_urls.py
+++ replace_urls.py
@@ -1,18 +1,23 @@
 import sys
-
-def relacingtexts(old,new):
-	with open('/var/www/html/index.html', 'r') as file :
-  		filedata = file.read()
-	
-    	# Replace the target string
-	filedata = filedata.replace(old, new)
-	
-	# Write the file out again
-	with open('/var/www/html/index.html', 'w') as file:
-	  	file.write(filedata)
-
+def replacingtexts(old1,new1):
+	try:
+	    with open('/var/www/html/index.html', 'r') as file :
+	  		filedata = file.read()		
+		# Replace the target string
+		filedata = filedata.replace(old1, new1)
+		# Write the file out again
+		with open('/var/www/html/index.html', 'w') as file:
+		    file.write(filedata)
+	except:
+		try:
+			with open('/var/www/html/index.html') as f:
+			    newText=f.read().replace(old1, new1)
+			with open('/var/www/html/index.html', "w") as f:
+			    f.write(newText)
+		except:
+			pass
 
 if __name__ == "__main__":
-    	new = str(sys.argv[1])
-    	old = "images"
-    	relacingtexts(old, new)
+    new = str(sys.argv[1])
+    old = "images"
+    replacingtexts(old, new)
